4462dc13709cb066e45d460ea6c7cbe2cb4ef1fa
Victor Vincent <wintercounter@gmail.com>
2015-07-23 09:40:02 +0200

    Target bugfix

--- src/Item.js
+++ src/Item.js
@@ -458,8 +458,8 @@
 			}
 
 			// If has target container
-			else if (target === C.SELECTOR_BODY && this.el.parents(C.SELECTOR_TARGET)) {
-				target = this.el.parents(C.SELECTOR_TARGET);
+			else if (target === C.SELECTOR_BODY && this.el.source.parents(C.SELECTOR_TARGET)) {
+				target = this.el.source.parents(C.SELECTOR_TARGET);
 			}
 
 			// Target is a selector
