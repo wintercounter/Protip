492f3f0192e26e970df490222a0552e2a07ee182
Victor Vincent <wintercounter@gmail.com>
2015-07-24 14:06:39 +0200

    Some mutation fixes

--- src/Class.js
+++ src/Class.js
@@ -351,9 +351,9 @@
 		 */
 		_mutationObserverCallback: function(mutations) {
 			mutations.forEach(function(mutation) {
+				var node;
 				for (var i = 0; i < mutation.addedNodes.length; i++) {
 					node = $(mutation.addedNodes[i]);
-
 					if (!node.hasClass(C.SELECTOR_PREFIX + C.SELECTOR_CONTAINER)) {
 						var els = node.parent().find(this.settings.selector);
 						els.each(function (index, el) {
