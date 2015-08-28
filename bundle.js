71717bdc277936c406f8155a3239e2987f105464
Travis <wintercounter@gmail.com>
2015-08-28 07:42:48 +0000

    Auto Build [ci skip]

--- bundle.js
+++ bundle.js
@@ -39,7 +39,8 @@ require('./src/Plugin');
 		window.MutationObserver._period = 100;
 	} catch(e){
 		console.error("Protip: MutationObserver polyfill haven't been loaded!");
-		window.MutationObserver = function(){this.disconnect=this.observe=function(){}};
+		// "Polyfill" for MutationObserver so Protip won't break if the real polyfill not included
+		window.MutationObserver = window.MutationObserver || function(){this.disconnect=this.observe=function(){}};
 	}
 
 	/**
