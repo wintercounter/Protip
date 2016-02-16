bd0d1bdf0eb5f4de817490aa6ad9ea8f74826d21
Victor Vincent <wintercounter@gmail.com>
2016-02-16 09:31:01 +0100

    Some timing. [ci skip]

--- site/js/site.js
+++ site/js/site.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
+    $.protip();
+    riot.mount('*', {
+        api: {
+            form: new Form().init()
+        }
+    });
+    $(window).resize();
     $('.js-loading-overlay').fadeOut(1000, function(){
         $(this).remove();
-        $.protip();
-        riot.mount('*', {
-            api: {
-                form: new Form().init()
-            }
-        });
     });
 });
